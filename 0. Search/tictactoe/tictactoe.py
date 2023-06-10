772f3e5101b88118bc04e556b09cadc80aad838c
Mikecamdo <mcdoherty@smu.edu>
2023-06-10 00:23:22 -0500

    Implemented first 3 TicTacToe functions
    Implemented the player(), actions(), and result() functions for the TicTacToe project

--- 0. Search/tictactoe/tictactoe.py
+++ 0. Search/tictactoe/tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -22,21 +23,48 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    if board == initial_state():
+        return X
+    else:
+        totalEmpty = 0 # The total number of empty squares left
+
+        for row in board:
+            totalEmpty += row.count(EMPTY)
+
+        if totalEmpty % 2 == 0:
+            return O
+        else:
+            return X
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    possibleMoves = set()
+
+    for i in range(3): # For each row of the board
+        for j in range(3): # For each square of the row
+            if board[i][j] == EMPTY: # If the square is empty, then the square is a possible move
+                possibleMoves.add((i, j))
+
+    return possibleMoves
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    newBoard = copy.deepcopy(board)
+    currentPlayer = player(board)
+    i, j = action
+
+    if newBoard[i][j] == EMPTY:
+        newBoard[i][j] = currentPlayer
+    else:
+        raise Exception("Not a valid move")
+
+    return newBoard
 
 
 def winner(board):
