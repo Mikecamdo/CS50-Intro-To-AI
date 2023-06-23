803bd061a7ab2bcf24b738f76e63c25dac312e34
Mikecamdo <mcdoherty@smu.edu>
2023-06-23 00:58:21 -0500

    Knights solution
    My initial solution to the 3 puzzles in the Knights project

--- 1. Knowledge/knights/puzzle.py
+++ 1. Knowledge/knights/puzzle.py
@@ -12,21 +12,45 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    #Information about the structure of the problem
+    Or(AKnight, AKnave), #A can be a Knight or a Knave
+    Not(And(AKnight, AKnave)), #A cannot be both a Knight and a Knave
+
+    #Information about what the character said
+    Implication(And(AKnight, AKnave), AKnight), #If what A said is true, then A is a Knight
+    Implication(Not(And(AKnight, AKnave)), AKnave) #If what A said is not true, then A is a Knave
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    #Information about the structure of the problem
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)),
+    Or(BKnight, BKnave),
+    Not(And(BKnight, BKnave)),
+
+    #Information about what the characters said
+    Implication(And(AKnave, BKnave), AKnight), #If A and B are both Knaves, then A is a Knight
+    Implication(Not(And(AKnave, BKnave)), AKnave) #If A and B are not both Knaves, then A is a Knave (and since the original statement must be false, B cannot also be a Knave)
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    #Information about the structure of the problem
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)),
+    Or(BKnight, BKnave),
+    Not(And(BKnight, BKnave)),
+
+    #Information about what the characters said
+    Implication(Or(And(AKnight, BKnight), And(AKnave, BKnave)), AKnight), #If what A said is true, A is a Knight
+    Implication(Not(Or(And(AKnight, BKnight), And(AKnave, BKnave))), AKnave), #If what A said is not true, A is a Knave
+    Implication(Or(And(AKnight, BKnave), And(AKnave, BKnight)), BKnight), #If what B said is true, then B is a Knight
+    Implication(Not(Or(And(AKnight, BKnave), And(AKnave, BKnight))), BKnave) #If what B said is not true, then B is a Knave
 )
 
 # Puzzle 3
@@ -35,7 +59,19 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    #Information about the structure of the problem
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)),
+    Or(BKnight, BKnave),
+    Not(And(BKnight, BKnave)),
+    Or(CKnight, CKnave),
+    Not(And(CKnight, CKnave)),
+
+    #Information about what the characters said
+    Or(Implication(AKnight, AKnight), Implication(AKnave, AKnight)), #A says either "I am a knight" or "I am a knave", but you don't know which
+    Implication(BKnight, Implication(AKnave, AKnight)), #B says “A said ‘I am a knave.’”
+    Biconditional(CKnave, BKnight), #B then says “C is a knave.”
+    Biconditional(AKnight, CKnight), #C says “A is a knight.”
 )
 
 
